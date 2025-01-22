66039e9401d231e76593893c993719af3b10dcd9
lacerdamadu <mariaeduardadulac@gmail.com>
2025-01-22 09:47:41 -0300

    Organizando os algoritmos de ordenação e a criação de uma Central

---
+++ Central.h
@@ -0,0 +1,8 @@
+#ifndef CENTRAL_H_
+#define CENTRAL_H_
+#include "Compartimento.h"
+#define STRING 100
+
+int CentralOrdena(Compartimento *lista);
+
+#endif
--- Compartimento.c
+++ Compartimento.c
@@ -24,3 +24,54 @@ int InsereRocha(Compartimento *ListaR, RochaMineral* NovaRocha){
     ListaR->Ultimo++;
     return 1;
 }
+
+void Insercao(Compartimento* lista, int tamanho){
+    int i,j;
+
+    RochaMineral aux;
+    
+    for (i = 1; i < tamanho; i++){
+        aux = lista->ListaRochas[i];
+        j = i - 1;
+        while ( ( j >= 0 ) && ( aux.Peso < lista[j].ListaRochas->Peso)){
+            lista[j + 1] = lista[j];
+            j--;
+        }
+    lista->ListaRochas[j + 1]= aux;
+ }
+}
+
+void QuickSort(Compartimento *lista, int n){
+    Ordena(0, n-1, lista);
+}
+void Ordena(int Esq, int Dir, Compartimento *lista){
+    int i,j;
+    Particao(Esq, Dir, &i, &j, lista);
+    if (Esq < j){
+        Ordena(Esq, j, lista);
+    } 
+    if (i < Dir) {
+        Ordena(i, Dir, lista);
+    }
+}
+void Particao(int Esq, int Dir,int *i, int *j, Compartimento *lista){
+    RochaMineral pivo, aux;
+    *i = Esq; *j = Dir;
+    pivo = lista->ListaRochas[(*i + *j)/2]; 
+
+    do
+    {
+        while (pivo.Peso > lista[*i].ListaRochas->Peso){
+            (*i)++;
+        } 
+        while (pivo.Peso < lista[*j].ListaRochas->Peso){
+            (*j)--;
+        }
+        if (*i <= *j){
+            aux = lista->ListaRochas[*i]; 
+            lista->ListaRochas[*i] = lista->ListaRochas[*j];
+            lista->ListaRochas[*j] = aux;
+            (*i)++; (*j)--;
+        }
+        } while (*i <= *j);
+}
